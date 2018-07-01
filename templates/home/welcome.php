1e03b46163ce44390cd07991e9cc9a35a72e1b96
Karsten Kaminski <karsten@kwd.co.za>
2018-07-01 10:32:22 +0200

    First commit

---
+++ templates/home/welcome.php
@@ -0,0 +1,12 @@
+<!-- Display welcome message -->
+<div class="outer-wrapper">
+  <div class="inner-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <?php $home = new Home(); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
